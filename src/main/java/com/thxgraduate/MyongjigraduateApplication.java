0fd6e87a40e87d472e166fd0bc921ddd8be5504b
baekjaehyuk <bjh010321@naver.com>
2025-08-08 17:12:28 +0900

    init

---
+++ src/main/java/com/thxgraduate/MyongjigraduateApplication.java
@@ -0,0 +1,13 @@
+package com.thxgraduate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyongjigraduateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyongjigraduateApplication.class, args);
+	}
+
+}
